6dfe94069896c677047b5902d6d8e1c2f8075c67
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-08-18 18:43:10 -0300

    feat: R10 :rocket:

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Title from './Title'; // arthurdebiasicopyright
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
+
+// const { name, year, country, destination } = missions;
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions
+            .map(({ name, year, country, destination }) => (
+              <MissionCard
+                key={ name }
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />
+            ))
+        }
       </div>
-    );
+    ); // arthurdebiasicopyright
   }
 }
